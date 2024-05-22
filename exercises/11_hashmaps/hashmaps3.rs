f4223e9430775d1e4677be690bf622d0a8bf0b77
zamion84 <zamion84@outlook.com>
2024-05-22 13:51:00 -0500

    hashmaps3 complete

--- exercises/11_hashmaps/hashmaps3.rs
+++ exercises/11_hashmaps/hashmaps3.rs
@@ -5,9 +5,9 @@
 // Example: England,France,4,2 (England scored 4 goals, France 2).
 //
 // You have to build a scores table containing the name of the team, the total
-// number of goals the team scored, and the total number of goals the team 
-// conceded. One approach to build the scores table is to use a Hashmap. 
-// The solution is partially written to use a Hashmap, 
+// number of goals the team scored, and the total number of goals the team
+// conceded. One approach to build the scores table is to use a Hashmap.
+// The solution is partially written to use a Hashmap,
 // complete it to pass the test.
 //
 // Make me pass the tests!
@@ -15,7 +15,6 @@
 // Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 use std::collections::HashMap;
 
@@ -35,17 +34,33 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         let team_1_score: u8 = v[2].parse().unwrap();
         let team_2_name = v[1].to_string();
         let team_2_score: u8 = v[3].parse().unwrap();
-        let t1 = Team{
-            goals_scored: team_1_score,
-            goals_conceded: team_2_score,
+        let mut t1 = Team {
+            goals_scored: 0,
+            goals_conceded: 0,
         };
-        scores.insert(team_1_name, t1);
-        let t2 = Team{
-            goals_scored: team_2_score,
-            goals_conceded: team_1_score,
+        let mut t2 = Team {
+            goals_scored: 0,
+            goals_conceded: 0,
         };
-        scores.insert(team_2_name, t2);
-        
+
+        scores
+            .entry(team_1_name)
+            .and_modify(|a| a.goals_scored += team_1_score)
+            .and_modify(|b| b.goals_conceded += team_2_score)
+            .or_insert(Team {
+                goals_scored: team_1_score,
+                goals_conceded: team_2_score,
+            });
+
+        scores
+            .entry(team_2_name)
+            .and_modify(|a| a.goals_scored += team_2_score)
+            .and_modify(|b| b.goals_conceded += team_1_score)
+            .or_insert(Team {
+                goals_scored: team_2_score,
+                goals_conceded: team_1_score,
+            });
+
         // TODO: Populate the scores table with details extracted from the
         // current line. Keep in mind that goals scored by team_1
         // will be the number of goals conceded by team_2, and similarly
