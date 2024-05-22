38d1ab5f0f8671ca1fb461ac9b3d7c1c7babcb2d
zamion84 <zamion84@outlook.com>
2024-05-22 18:57:08 -0500

    wip

--- exercises/13_error_handling/errors1.rs
+++ exercises/13_error_handling/errors1.rs
@@ -11,7 +11,7 @@
 
 // I AM NOT DONE
 
-pub fn generate_nametag_text(name: String) -> Option<String> {
+pub fn generate_nametag_text(name: String) -> Result<String, String> {
     if name.is_empty() {
         // Empty names aren't allowed.
         None
