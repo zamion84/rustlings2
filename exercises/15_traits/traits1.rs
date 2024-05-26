1745d89ce925fd233544bc06c75f1e32add061bc
zamion84 <zamion84@outlook.com>
2024-05-25 21:40:50 -0500

    Errors complete

--- exercises/15_traits/traits1.rs
+++ exercises/15_traits/traits1.rs
@@ -15,6 +15,10 @@ trait AppendBar {
 
 impl AppendBar for String {
     // TODO: Implement `AppendBar` for type `String`.
+    fn append_bar(self) -> Self {
+        self.push("{}Bar",self)
+    }
+    
 }
 
 fn main() {
