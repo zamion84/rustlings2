268dff4b68c0ae38918d1294768cd70e270ad9ff
zamion84 <zamion84@outlook.com>
2024-05-27 15:37:45 -0500

    smart pointers complete

--- exercises/18_iterators/iterators4.rs
+++ exercises/18_iterators/iterators4.rs
@@ -3,9 +3,9 @@
 // Execute `rustlings hint iterators4` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 pub fn factorial(num: u64) -> u64 {
+
+    (1..=num).product()
     // Complete this function to return the factorial of num
     // Do not use:
     // - early returns (using the `return` keyword explicitly)
